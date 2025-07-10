31d8e981cc016aadf9b80080420ac6a996f67a08
vasilerimari <vasile.rimari@gmail.com>
2025-07-10 09:49:29 +0200

    app.py

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ from datetime import datetime, timedelta
 from collections import defaultdict, deque
 from utils_confirmari import calculate_rsi, calculate_cvd, detect_market_structure
 
-POLYGON_API_KEY = st.secrets["POLYGON_API_KEY"]
+POLYGON_API_KEY = "NcRnNhMkfPDwv_gCgMDCKwgnpXb0jLnM"
+
 TELEGRAM_WEBHOOK_URL = st.secrets.get("TELEGRAM_WEBHOOK_URL", "")
 
 WATCHLIST = [
